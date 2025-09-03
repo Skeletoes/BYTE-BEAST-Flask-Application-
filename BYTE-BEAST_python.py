990e9bb3a35c886df52b70bae6a553cdc0983a67
Brenton Graham <23674@burnside.school.nz>
2025-09-03 14:23:52 +1200

    Add initial implementation of BYTE BEAST Flask application with CPU, memory, network, and disk monitoring features

---
+++ BYTE-BEAST_python.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, jsonify
+import psutil
+import time
+app = Flask(__name__)
+cpu_stats = []
+cpu_time = []
+memory_stats = []
+memory_time=[]
+networkIn_stats = []
+networkOut_stats = []
+network_time = []
+diskRead_stats = []
+diskWrite_stats = []
+disk_time = []
+
+@app.route('/')
+def homepage():
+    return render_template('BYTE-BEAST_html.html')
+
+@app.route('/cpu-data')
+def cpu_data():
+    cpu_percent = psutil.cpu_percent(interval=0.1)
+    cpu_stats.append(cpu_percent)
+
+    cpu_time1 = time.time()
+    cpu_time2 = time.ctime(cpu_time1)
+    cpu_time.append(cpu_time2[11:19])
+    
+    cpu_data = cpu_time[-30:]
+    cpu_labels = cpu_stats[-30:]
+    time.sleep(0.2)
+
+    return jsonify({'cpu_labels': cpu_labels, 'cpu_data': cpu_data})
+
+@app.route('/memory-data')
+def memory_data():
+    memory_percent = psutil.virtual_memory().percent
+    memory_stats.append(memory_percent)
+
+    memory_time1 = time.time()
+    memory_time2 = time.ctime(memory_time1)
+    memory_time.append(memory_time2[11:19])
+
+    memory_data = memory_time[-30:]
+    memory_labels = memory_stats[-30:]
+    time.sleep(0.2)
+
+    return jsonify({'memory_labels': memory_labels, 'memory_data': memory_data})
+
+@app.route('/network-data')
+def network_data():
+    network = psutil.net_io_counters()
+    bytesIn1 = network[0]
+    bytesOut1 = network[1]
+    time.sleep(0.1)
+    network = psutil.net_io_counters()
+    bytesIn2 = network[0]
+    bytesOut2 = network[1]
+    time.sleep(0.1)
+    
+    bytesIn = bytesIn2 - bytesIn1
+    bytesOut = bytesOut2 - bytesOut1
+
+    networkIn_stats.append(bytesIn)
+    networkOut_stats.append(bytesOut)
+
+    network_time1 = time.time()
+    network_time2 = time.ctime(network_time1)
+    network_time.append(network_time2[11:19])
+
+    network_data = network_time[-30:]
+    networkIn_labels = networkIn_stats[-30:]
+    networkOut_labels = networkOut_stats[-30:]
+
+    return jsonify({'networkIn_labels': networkIn_labels, 'networkOut_labels': networkOut_labels, 'network_data': network_data})
+
+@app.route('/disk-data')
+def disk_data():
+    disk = psutil.disk_io_counters()
+    bytes_read1 = disk.read_bytes
+    bytes_write1 = disk.write_bytes
+    time.sleep(0.1)
+    disk = psutil.disk_io_counters()
+    bytes_read2 = disk.read_bytes
+    bytes_write2 = disk.write_bytes
+    time.sleep(0.1)
+
+    bytes_read = bytes_read2 - bytes_read1
+    bytes_write = bytes_write2 - bytes_write1
+
+    diskRead_stats.append(bytes_read)
+    diskWrite_stats.append(bytes_write)
+
+    diskTime1 = time.time()
+    diskTime2 = time.ctime(diskTime1)
+    disk_time.append(diskTime2[11:19])
+
+    disk_data = disk_time[-30:]
+    diskRead_labels = diskRead_stats[-30:]
+    diskWrite_labels = diskWrite_stats[-30:]
+
+    return jsonify({'diskRead_labels': diskRead_labels, 'diskWrite_labels': diskWrite_labels, 'disk_data': disk_data})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/BYTE-BEAST_javascript.js
@@ -0,0 +1,212 @@
+const CPU_graph = document.getElementById('CPU_chart').getContext('2d');
+
+const CPU_config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'CPU Usage (%)',
+            backgroundColor: 'rgb(4, 170, 109)',
+            borderColor: 'rgb(4, 170, 109)',
+            data: [],
+            fill: true,
+            tension: 0.3,
+            animation: false,
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: { color: '#ccc' }
+            }
+        }
+    }
+};
+
+const CPU_chart = new Chart(CPU_graph, CPU_config);
+
+const MEMORY_graph = document.getElementById('MEMORY_chart').getContext('2d');
+
+const MEMORY_config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Memory Usage (%)',
+            backgroundColor: 'rgb(4, 170, 109)',
+            borderColor: 'rgb(4, 170, 109)',
+            data: [],
+            fill: true,
+            tension: 0.3,
+            animation: false,
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: { color: '#ccc' }
+            }
+        }
+    }
+};
+
+const MEMORY_chart = new Chart(MEMORY_graph, MEMORY_config);
+
+const NETWORK_graph = document.getElementById('NETWORK_chart').getContext('2d');
+
+const NETWORK_config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Bytes Received',
+            backgroundColor: 'rgb(4, 100, 109)',
+            borderColor: 'rgb(4, 100, 109)',
+            data: [],
+            fill: false,
+            tension: 0.3,
+            animation: false,
+        },{
+            label: 'Bytes Sent',
+            backgroundColor: 'rgb(4, 255, 109)',
+            borderColor: 'rgb(4, 255, 109)',
+            data: [],
+            fill: false,
+            tension: 0.3,
+            animation: false,
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: { color: '#ccc' }
+            }
+        }
+    }
+};
+
+const NETWORK_chart = new Chart(NETWORK_graph, NETWORK_config);
+
+const DISK_graph = document.getElementById('DISK_chart').getContext('2d');
+
+const DISK_config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Bytes Read',
+            backgroundColor: 'rgb(4, 100, 109)',
+            borderColor: 'rgb(4, 100, 109)',
+            data: [],
+            fill: false,
+            tension: 0.3,
+            animation: false,
+        },{
+            label: 'Bytes Written',
+            backgroundColor: 'rgb(4, 255, 109)',
+            borderColor: 'rgb(4, 255, 109)',
+            data: [],
+            fill: false,
+            tension: 0.3,
+            animation: false,
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: { color: '#ccc' },
+                grid: { color: '#333' }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: { color: '#ccc' }
+            }
+        }
+    }
+};
+
+const DISK_chart = new Chart(DISK_graph, DISK_config);
+
+function fetchData() {
+    fetch('/cpu-data')
+        .then(response => response.json())
+        .then(json => {
+            CPU_chart.data.labels = json.cpu_data;
+            CPU_chart.data.datasets[0].data = json.cpu_labels;
+            CPU_chart.update();
+        });
+    fetch('/memory-data')
+        .then(response => response.json())
+        .then(json => {
+            MEMORY_chart.data.labels = json.memory_data;
+            MEMORY_chart.data.datasets[0].data = json.memory_labels;
+            MEMORY_chart.update();
+        });
+    fetch('/network-data')
+        .then(response => response.json())
+        .then(json => {
+            NETWORK_chart.data.labels = json.network_data;
+            NETWORK_chart.data.datasets[0].data = json.networkIn_labels;
+            NETWORK_chart.data.datasets[1].data = json.networkOut_labels;
+            NETWORK_chart.update();
+        });
+    fetch('/disk-data')
+        .then(response => response.json())
+        .then(json => {
+            DISK_chart.data.labels = json.disk_data;
+            DISK_chart.data.datasets[0].data = json.diskRead_labels;
+            DISK_chart.data.datasets[1].data = json.diskWrite_labels;
+            DISK_chart.update();
+        });
+}
+
+
+
+// Initial fetch
+fetchData();
+
+// Poll every second
+setInterval(fetchData, 1000);
